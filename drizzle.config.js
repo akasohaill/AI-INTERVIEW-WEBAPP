03c2186a0adf0331606119e911a6b00063e02aa8
Md Sohail Ansari <121673553+akasohaill@users.noreply.github.com>
2024-06-16 20:32:18 +0530

    Delete drizzle.config.js

--- drizzle.config.js
+++
@@ -1,8 +0,0 @@
-/** @type { import("drizzle-kit").Config } */
-export default {
-    schema: "./utils/schema.js",
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: 'postgresql://ai-mocker_owner:0F1BZQwSONnd@ep-winter-flower-a1x30uj6.ap-southeast-1.aws.neon.tech/ai-mocker?sslmode=require',
-    }
-  };
